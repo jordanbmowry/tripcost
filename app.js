b8e9a92706b9d0dbc038ae3f5b9c57d06dbcfce0
Jordan Mowry <jordan.mowry@gmail.com>
2021-06-02 10:39:21 -0700

    made the add an expense functionality on endpoint /expense

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const mongo = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
 
 const app = express();
 app.use(express.json());
-
-const url = 'mongodb://localhost:27017';
 
 let db;
 
@@ -39,11 +38,34 @@ app.post('/trip', (req, res) => {
 });
 
 app.get('/trips', (req, res) => {
-	/* */
+	trips.find().toArray((err, items) => {
+		if (err) {
+			console.error(err);
+			res.status(500).json({ err: err });
+			return;
+		}
+		res.status(200).json({ trips: items });
+	});
 });
 
 app.post('/expense', (req, res) => {
-	/* */
+	expenses.insertOne(
+		{
+			trip: req.body.trip,
+			date: req.body.date,
+			amount: req.body.amount,
+			category: req.body.category,
+			description: req.body.description,
+		},
+		(err, result) => {
+			if (err) {
+				console.error(err);
+				res.status(500).json({ err: err });
+				return;
+			}
+			res.status(200).json({ ok: true });
+		}
+	);
 });
 
 app.get('/expenses', (req, res) => {
