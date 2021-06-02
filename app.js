e091cb4f45354cf228ab0b9f040f47129e2fc1ce
Jordan Mowry <jordan.mowry@gmail.com>
2021-06-02 09:12:12 -0700

    brought it express.json middleware and added trip to database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongo = require('mongodb').MongoClient;
 
 const app = express();
+app.use(express.json());
 
 const url = 'mongodb://localhost:27017';
 
@@ -25,14 +26,18 @@ mongo.connect(
 );
 
 app.post('/trip', (req, res) => {
-	/* */
+	const name = req.body.name;
+	trips.insertOne({ name: name }, (err, result) => {});
 });
+
 app.get('/trips', (req, res) => {
 	/* */
 });
+
 app.post('/expense', (req, res) => {
 	/* */
 });
+
 app.get('/expenses', (req, res) => {
 	/* */
 });
